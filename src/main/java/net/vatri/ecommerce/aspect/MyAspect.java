5bc211386e03d3f0825188863dbf9aa9c3110a92
Harsh Dubey <111901053@smail.iitpkd.ac.in>
2023-03-07 19:44:39 +0530

    First commit

---
+++ src/main/java/net/vatri/ecommerce/aspect/MyAspect.java
@@ -0,0 +1,26 @@
+package net.vatri.ecommerce.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAspect {
+    @AfterThrowing("execution(* net.vatri.ecommerce.cart.CartServiceImpl.addProduct(*, *))")
+    public void afterThrowing(JoinPoint joinPoint) {
+        System.out.println("Method: " + joinPoint.toString());
+        System.out.println("args: ");
+        for (Object o :
+                joinPoint.getArgs()) {
+            System.out.println(o.toString());
+        }
+    }
+
+    @Before("execution(* net.vatri.ecommerce.cart.CartServiceImpl.addProduct(*, *))")
+    public void printBefore() {
+        System.out.println("MY FUNCTION STARTED .......");
+    }
+}
