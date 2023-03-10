908994c633bf5834e86f1924cbd105844d84e2f0
Satyam Mishra <111901058@smail.iitpkd.ac.in>
2023-03-10 13:25:51 +0530

    client server code added

--- src/main/java/net/vatri/ecommerce/aspect/MyAspect.java
+++ src/main/java/net/vatri/ecommerce/aspect/MyAspect.java
@@ -1,27 +1,56 @@
 package net.vatri.ecommerce.aspect;
 
+import net.vatri.ecommerce.cart.CartItem;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 @Aspect
 @Component
 public class MyAspect {
-    @AfterThrowing("execution(* net.vatri.ecommerce.cart.CartServiceImpl.addProduct(*, *))")
-    public void afterThrowing(JoinPoint joinPoint) {
-        System.out.println("Method: " + joinPoint.toString());
-        System.out.println("args: ");
-        for (Object o:
-                joinPoint.getArgs()) {
-            System.out.println(o.toString());
-        }
+
+    DataOutputStream outputStream;
+
+    public MyAspect() throws IOException {
+        Socket s = new Socket("localhost",6666);
+        System.out.println("connection to the server made");
+        outputStream = new DataOutputStream(s.getOutputStream());
     }
 
-//    @Before("execution(* net.vatri.ecommerce.controllers.CartController.addProduct(*, *))")
+    @AfterThrowing("execution(* net.vatri.ecommerce.cart.CartServiceImpl.addProduct(*, *))")
+    public void afterThrowing(JoinPoint joinPoint) throws IOException {
+        String msgString = "";
+//        System.out.println("Method: " + joinPoint.toString());
+        msgString += "Method: " + joinPoint.toString() + "\n";
+//        System.out.println("args: ");
+        msgString += "args: " + "\n";
+        for (Object o:
+                joinPoint.getArgs()) {
+//            System.out.println(o.toString());
+            if (o instanceof CartItem) {
+                CartItem cartItem = (CartItem) o;
+                msgString += "product Id: " + cartItem.getProductId() + "\n";
+                msgString += "quantity Id: " + cartItem.getQuantity() + "\n";
+                msgString += "variant Id: " + cartItem.getVariantId() + "\n";
+            }
+            else {
+                msgString += o.toString() + "\n";
+            }
+        }
+        outputStream.writeUTF(msgString);
+        outputStream.flush();
+    }
+
     @Before("execution(* net.vatri.ecommerce.cart.CartService.addProduct(*, *))")
-    public void printBefore() {
-        System.out.println("MY FUNCTION STARTED .......");
+    public void printBefore() throws IOException {
+//        System.out.println("MY FUNCTION STARTED .......");
+        outputStream.writeUTF("MY FUNCTION STARTED .......");
+        outputStream.flush();
     }
 }
